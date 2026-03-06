a32e3370b860dffc79689405c5679def1ab56b7b
kleryxx <126974469+Vlad-Lipinskyi@users.noreply.github.com>
2026-03-05 19:46:45 -0500

    + logger

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import asyncio
 import random
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
 from aiogram import F
@@ -11,7 +11,22 @@ TOKEN = "8587415331:AAHzZpuSQSXL4cT67u-p1pvNxSWo4DbqWK8"
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    filename="bot.log",
+    encoding="utf-8",
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
+def log_user_action(message: Message, action: str):
+    user = message.from_user
+
+    if user.username:
+        user_info = f"@{user.username}({user.full_name}) {user.id}"
+    else:
+        user_info = f"{user.full_name} {user.id}"
+
+    logging.info(f"{user_info} натиснув кнопку {action}")
 
 compliments = [
     "Ти найкраща мама у світі 💖",
@@ -38,6 +53,8 @@ keyboard = ReplyKeyboardMarkup(
 
 @dp.message(Command("start"))
 async def start_handler(message: Message):
+    log_user_action(message, "/start")
+
     await message.answer(
         "Привіт, мама 💐\nЦей бот створений спеціально для тебе ❤️\nЗ 8 березня 🌷",
         reply_markup=keyboard
@@ -45,21 +62,30 @@ async def start_handler(message: Message):
 
 @dp.message(F.text == "🌷 Комплімент")
 async def send_compliment(message: Message):
+    log_user_action(message, "🌷 Комплімент")
+
     await message.answer(random.choice(compliments))
 
 @dp.message(F.text == "💖 Подяка")
 async def send_thanks(message: Message):
+    log_user_action(message, "💖 Подяка")
+
     await message.answer(random.choice(thanks))
 
 @dp.message(Command("compliment"))
 async def compliment_command(message: Message):
+    log_user_action(message, "/compliment")
+
     await message.answer(random.choice(compliments))
 
 @dp.message(Command("thanks"))
 async def thanks_command(message: Message):
+    log_user_action(message, "/thanks")
+
     await message.answer(random.choice(thanks))
 
 async def main():
+    logging.info("Бот запущено")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
